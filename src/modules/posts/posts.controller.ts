283246e83cc458d56d7c2065194f8fc2eaa1ca4b
imSeveNyU <fivesevenyu@163.com>
2020-01-03 14:48:58 +0800

    核心组件3

--- src/modules/posts/posts.controller.ts
+++ src/modules/posts/posts.controller.ts
@@ -14,26 +14,37 @@ import {
   ParseIntPipe,
   UseGuards,
   SetMetadata,
+  UseInterceptors,
+  Req,
 } from '@nestjs/common';
 import { CreatePostDto } from './post.dto';
 import { DemoService } from './providers/demo/demo.service';
 import { DemoFilter } from 'src/core/filters/demo.filter';
 import { DemoAuthGuard } from 'src/core/guards/demo-auth.guard';
 import { Roles } from 'src/core/decorators/roles.decorator';
+import { LoggingInterceptor } from 'src/core/interceptors/logging.interceptor';
+import { TransformInterceptor } from 'src/core/interceptors/transform.interceptor';
+import { ErrorsInterceptor } from 'src/core/interceptors/errors.interceptor';
+import { User } from 'src/core/decorators/user.decorator';
+import { DemoPipe } from 'src/core/pipes/demo.pipe';
 
 @Controller('posts')
 // @UseFilters(DemoFilter)
 // @UseGuards(DemoAuthGuard)
+// @UseInterceptors(LoggingInterceptor)
 export class PostsController {
   constructor(private readonly demoService: DemoService) {}
 
   @Get()
+  // @UseInterceptors(TransformInterceptor)
+  @UseInterceptors(ErrorsInterceptor)
   index(@Headers('authorization') headers) {
-    return this.demoService.findAll();
+    throw new ForbiddenException()
+    // return this.demoService.findAll();
   }
 
   @Get(':id')
-  show(@Param('id', ParseIntPipe) id) {
+  show(@Param('id', ParseIntPipe, DemoPipe) id) {
     console.log('id:', typeof id);
 
     return {
@@ -46,7 +57,8 @@ export class PostsController {
   @UsePipes(ValidationPipe)
   // @SetMetadata('roles', ['member'])
   @Roles('member')
-  store(@Body() post: CreatePostDto) {
+  store(@Body()  post: CreatePostDto, @User('demo') user) {
+    console.log(user)
     // throw new HttpException('没有权限', HttpStatus.FORBIDDEN)
     // throw new ForbiddenException('没有权限 ！')
     this.demoService.create(post);
